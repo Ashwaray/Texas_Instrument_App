df3133cf2acefc632c2902337fa7ce689b9721b0
Ashwaray <ashwarayraj@gmail.com>
2015-03-15 18:07:38 +0530

    csv write

--- ss.py
+++ ss.py
@@ -1,4 +1,11 @@
 import serial
 import csv
 
-#ser = serial.Serial("COM8", 9600)
+data = serial.Serial("COM8", 9600)
+
+path = "data.csv"
+
+b = open(path, "wb")
+a = csv.writer(b)
+a.writerows(data)
+b.close()
